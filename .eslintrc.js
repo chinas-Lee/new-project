12ec64fb340ba796c78a15b5db458a2f5d3ce7d4
lcq <lichaoqiangyes@163.com>
2019-06-11 15:40:33 +0800

    change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-const-assign': 2
     },
     parserOptions: {
         parser: 'babel-eslint'
